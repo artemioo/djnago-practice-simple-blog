95fc0dc985f9903bd1194cdce3f59b6ee1afb9d8
artemioo <artemryb1@gmail.com>
2021-09-11 12:54:23 +0300

    added mixin and Tag model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True) #ставим индексирование для быстрого поиска
     slug = models.SlugField(max_length=150, unique=True) #уник. поля по умолчанию индексируются
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateField(auto_now_add=True)
 
     def get_absolute_url(self):
@@ -13,3 +14,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title  #переопредили метод str для отображения
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
+from .utils import ObjectDetailMixin
+from django.views.generic import View
 
 def main_view(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
-def post_view(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags':tags})
